e4a2fd91c53001613c9022e0308b1a77617b5967
Fernando G Almeida <alessgarcia.fernando@gmail.com>
2022-07-17 17:39:35 -0300

    change settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'cisco'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['agile-ocean-32203.herokuapp.com','127.0.0.1']
 
 
 # Application definition
